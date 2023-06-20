040767372233a00c42deac13d0b9afa1eb13663b
iviniciuss7 <oliveira.igor@escolar.ifrn.edu.br>
2023-06-20 14:24:38 -0300

    MovimentaçãoPlayer
    arquivos com mov do player

---
+++ JOGOoficinaIgoreEraldo/Assets/Scripts/PLAYER/movplayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movplayer : MonoBehaviour
+{
+    public float speed;
+    public Vector2 position;
+    public Rigidbody2D rig;
+    void Start()
+    {
+
+   
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
