fbdfe272173aa929863a4d97c3137a7f121c06ed
iviniciuss <oliveira.igor@escolar.ifrn.edu.br>
2023-06-27 14:34:02 -0300

    Movimentação Do Player
    MovMais pulo

--- JOGOoficinaIgoreEraldo/Assets/Scripts/PLAYER/movplayer.cs
+++ JOGOoficinaIgoreEraldo/Assets/Scripts/PLAYER/movplayer.cs
@@ -1,21 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class movplayer : MonoBehaviour
 {
+    public float forcadoPulo;
     public float speed;
-    public Vector2 position;
-    public Rigidbody2D rig;
+    private Animator anim;
+    private Rigidbody2D rig;
+    public bool noAr;
+    public bool puloDuplo;
     void Start()
     {
-
-   
-}
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Movimentacao();
+        Pulo();
+    }
+
+    void Movimentacao()
+    {
+        float movimento = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(movimento * speed, rig.velocity.y);
+
+        if (movimento > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (movimento < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+    
+    void Pulo()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!noAr)
+            {
+                rig.AddForce(new Vector2(0, forcadoPulo), ForceMode2D.Impulse);
+                puloDuplo = true;
+                noAr = true;
+            }
+            
         
+            else
+            {
+                rig.AddForce(new Vector2(0, forcadoPulo * 2), ForceMode2D.Impulse);
+                puloDuplo = false;
+            }
+        }
+    }    
+        void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 3)
+        {
+            noAr = false;
+        }
     }
 }
