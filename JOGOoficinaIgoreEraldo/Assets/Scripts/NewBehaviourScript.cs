27f295e7fa753b752c7588e4356c329ddbb60891
iviniciuss7 <oliveira.igor@escolar.ifrn.edu.br>
2023-07-29 00:06:53 -0300

    CriacaoDaCena
    ACriação.

---
+++ JOGOoficinaIgoreEraldo/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Transform player;
+    public float suavidade;
+    public float camlimitx1;
+    public float camlimitx2;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        if (player.position.x >= camlimitx1 && player.position.x <= camlimitx2)
+        {
+            Vector3 seguindo = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, seguindo, suavidade * Time.deltaTime);
+        }
+        
+    }
+}
