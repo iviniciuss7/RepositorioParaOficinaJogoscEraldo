22b1d9c2162597168d5ea7e4095e4b0d850964cb
Speedzyy <eraldo.soares@escolar.ifrn.edu.br>
2023-08-03 00:30:03 -0300

    commit leve, só mudou o colisor da guitarra que muda de cena

--- JOGOoficinaIgoreEraldo/Assets/Scripts/OTHERS/SpecialPower.cs
+++ JOGOoficinaIgoreEraldo/Assets/Scripts/OTHERS/SpecialPower.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class SpecialPower : MonoBehaviour
 {
-    [SerializeField]float maximo;
-    [SerializeField]float atual;
+    [SerializeField] float maximo;
+    [SerializeField] float atual;
     [SerializeField] UnityEngine.UI.Image fill;
+
+    private bool poderEspecialAtivado = false;
 
     void Update()
     {
-        fill.fillAmount = atual / maximo;
+        if (poderEspecialAtivado)
+        {
+            // Exemplo: Código que será executado quando o poder especial estiver ativado.
+            // Por exemplo, você pode fazer o personagem ficar mais forte ou realizar uma animação especial.
+            // Exemplo: personagem.AtivarPoderEspecial();
+            // Exemplo: animacaoAtivarPoder.Play();
+            // Exemplo: ReiniciarBarra();
+        }
+        else
+        {
+            fill.fillAmount = atual / maximo;
 
+            if (fill.fillAmount >= 1.0f)
+            {
+                AtivarPoderEspecial();
+            }
+        }
+    }
 
+    private void AtivarPoderEspecial()
+    {
+        // Exemplo: Código que será executado quando o poder especial for ativado.
+        // Por exemplo, você pode fazer o personagem ficar mais forte ou realizar uma animação especial.
+        // Exemplo: personagem.FicarMaisForte();
+        // Exemplo: animacaoAtivarPoder.Play();
+        // Exemplo: ReiniciarBarra();
+
+        // Reinicia a barra para permitir a recarga do poder especial.
+        ReiniciarBarra();
+    }
+
+    private void ReiniciarBarra()
+    {
+        // Reinicia a barra de poder.
+        atual = 0f;
+        fill.fillAmount = 0f;
     }
 }
